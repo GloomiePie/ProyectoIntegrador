d48a79ab9b5720bfcc745843082ca23d4eaaf891
CarlosMC2003 <92834288+CarlosMC2003@users.noreply.github.com>
2023-02-02 16:40:00 -0500

    Avance - Script / Segunda Prueba
    Segunda Prueba del Script para poblar la base de datos

--- Main4.scala
+++ Main4.scala
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths, StandardOpenOption}
 
 object Main4 extends App{
-  val reader = CSVReader.open(new File("C:\\Users\\CM\\movie_dataset.csv"))
+  val reader = CSVReader.open(new File("C:\\Users\\SALA A\\Downloads\\movie_dataset.csv"))
   val data = reader.allWithHeaders()
   reader.close()
 
@@ -23,20 +23,27 @@ object Main4 extends App{
     .replaceAll("\"", "\\\\\"")
 
   case class Movie(
-                  id: String,
-                  original_language: String,
-                  original_title: String,
-                  budget: Long,
-                  popularity: Double,
-                  runtime: Double,
-                  revenue: Long
+                    id: Int,
+                    index: Int,
+                    original_language: String,
+                    original_title: String,
+                    overview: String,
+                    budget: Long,
+                    popularity: Double,
+                    runtime: Double,
+                    revenue: Long
                   )
 
   val movieData = data
     .map(row => Movie(
-      row("id"),
+      row("id").toInt,
+      row("index").toInt,
       row("original_language"),
       row("original_title"),
+      row("overview") match {
+        case valueOfOver if valueOfOver.trim.isEmpty => "No registrado"
+        case valueOfOver => valueOfOver
+      },
       row("budget").toLong,
       row("popularity").toDouble,
       row("runtime") match {
@@ -46,16 +53,18 @@ object Main4 extends App{
       row("revenue").toLong))
 
   val SQL_INSERT_PATTERN =
-    """INSERT INTO MOVIE ('id', 'original_language', 'original_title', 'budget', 'popularity', 'runtime', 'revenue')
+    """INSERT INTO MOVIE ('id', 'index ', 'original_language', 'original_title', 'overview', 'budget', 'popularity', 'runtime', 'revenue')
       |VALUES
-      |('%s', '%s', '%s', '%d', '%f', '%f', '%d')
+      |('%s', %d ,'%s', '%s', '%s', '%d', '%f', '%f', '%d')
       |""".stripMargin
 
   val scriptData = movieData
     .map(movie => SQL_INSERT_PATTERN.formatLocal(java.util.Locale.US,
       movie.id,
+      movie.index,
       escapeMysql(movie.original_language),
       escapeMysql(movie.original_title),
+      escapeMysql(movie.overview),
       movie.budget,
       movie.popularity,
       movie.runtime,
@@ -66,6 +75,6 @@ object Main4 extends App{
   if(ScriptFile.exists()) ScriptFile.delete()
 
   scriptData.foreach(insert =>
-    Files.write(Paths.get("C:\\Users\\CM\\Desktop\\movies_insert.sql"), insert.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.APPEND)
+    Files.write(Paths.get("C:\\Users\\SALA A\\Desktop\\movies_insert.sql"), insert.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.APPEND)
   )
 }
