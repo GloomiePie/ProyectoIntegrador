ea936c21363528964dd991c9cf224c6a055ecd36
Pablo <92599790+GloomiePie@users.noreply.github.com>
2023-02-05 10:00:27 -0500

    Corrección de la inserción de crew

--- SentenciasInsertInto.scala
+++ SentenciasInsertInto.scala
@@ -1,22 +1,20 @@
-package ProyectoIntegrador
-
 import java.io.File
 import com.github.tototoshi.csv.CSVReader
 import io.circe.JsonObject
 import scalikejdbc._
 import play.api.libs.json._
-import requests.Response
+import requests.{Response, readTimeout}
 
-import scala.util.{Success, Try}
+import scala.util.{Failure, Success, Try}
 import scala.util.matching.Regex
 
 object SentenciasInsertInto extends App {
-  val reader = CSVReader.open(new File("C:\\Users\\CM\\movie_dataset.csv"))
+  val reader = CSVReader.open(new File("C:\\Users\\USUARIO\\Downloads\\movie_dataset.csv"))
   val data = reader.allWithHeaders()
   reader.close()
 
   Class.forName("com.mysql.cj.jdbc.Driver")
-  ConnectionPool.singleton("jdbc:mysql://localhost:3306/ProyectoIntegrador", "root", "nomeacuerdo")
+  ConnectionPool.singleton("jdbc:mysql://localhost:3306/ProyectoIntegrador", "root", "29122003g?")
   implicit val session: DBSession = AutoSession
 
   //COLUMNA GENRES - Pasado A Base De Datos
@@ -27,10 +25,9 @@ object SentenciasInsertInto extends App {
     .flatMap(x => x._2.map((x._1, _)))
     .map(x => (x._1.toInt, x._2))
     .sortBy(_._1)
-
     generos.map(elem =>
     sql"""
-         |INSERT INTO genres(idMovie, name)
+         |INSERT INTO Genres (idMovie, name)
          |VALUES
          |(${elem._1}, ${elem._2})
                """.stripMargin
@@ -38,14 +35,13 @@ object SentenciasInsertInto extends App {
       .apply())*/
 
   //COLUMNA CAST - Pasado A Base De Datos
-  /*val cast = data
+  val cast = data
     .map(elem => (elem("id"), elem("cast")))
     .filter(_._2.nonEmpty)
     .map(x => (x._1, x._2.split(" ")))
     .flatMap(x => x._2.map((x._1, _)))
     .map(x => (x._1.toInt, x._2))
     .sortBy(_._1)
-
   cast.map(elem =>
     sql"""
          |INSERT INTO Cast (idMovie, Name)
@@ -53,17 +49,16 @@ object SentenciasInsertInto extends App {
          |(${elem._1}, ${elem._2})
                  """.stripMargin
       .update
-      .apply())*/
+      .apply())
 
-  //COLUMNA KEYWORDS
-  /*val claves = data
+  //COLUMNA KEYWORDS - Pasado a base de datos
+  val claves = data
     .map(elem => (elem("id"), elem("keywords")))
     .filter(_._2.nonEmpty)
     .map(x => (x._1, x._2.split(" ")))
     .flatMap(x => x._2.map((x._1, _)))
     .map(x => (x._1.toInt, x._2))
     .sortBy(_._1)
-
   claves.map(elem =>
     sql"""
          |INSERT INTO Keywords (idMovie, Name)
@@ -71,14 +66,12 @@ object SentenciasInsertInto extends App {
          |(${elem._1}, ${elem._2})
                    """.stripMargin
       .update
-      .apply())*/
+      .apply())
 
   //COLUMNA production_companies
   /*val productionCompanies = data
     .map(elem => (elem("id"), elem("production_companies")))
-
   println(productionCompanies)
-
   productionCompanies.map(elem =>
     sql"""
          |INSERT INTO production_companies (idMovie, name, idCompany)
@@ -88,12 +81,10 @@ object SentenciasInsertInto extends App {
       .update
       .apply())*/
 
-   //COLUMNA production_countries
+  //COLUMNA production_countries
   /*val productionCountries = data
     .map(elem => (elem("id"), elem("production_countries")))
-
   println(productionCountries)
-
   productionCountries.map(elem =>
     sql"""
          |INSERT INTO production_countries (idMovie, name, `iso_3166_1`)
@@ -106,9 +97,7 @@ object SentenciasInsertInto extends App {
   //COLUMNA spoken_language
   /*val spoken_language = data
     .map(elem => (elem("id"), elem("spoken_languages")))
-
   println(spoken_language)
-
   spoken_language.map(elem =>
     sql"""
          |INSERT INTO spoken_language (idMovie, `iso_639_1`, name)
@@ -118,28 +107,33 @@ object SentenciasInsertInto extends App {
       .update
       .apply())*/
 
-  //COLUMNA Crew
-  //Error Desconocido
   val pattern1 = "(\\s\"(.*?)\",)".r
   val pattern2 = "([a-z]\\s\"(.*?)\"\\s*[A-Z])".r
   val pattern3 = "(:\\s'\"(.*?)',)".r
 
   def replacePattern4(original: String, pattern: Regex): String = {
     var txtOr = original
+
     for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      var replaceText = m
       if (pattern == pattern2) {
-        txtOr = txtOr.replace(m, m.replace("\"", "-u0022"))
+        replaceText = m.replace("\"", "-u0022")
       } else if (pattern == pattern1) {
-        txtOr = txtOr.replace(m, m.replace("'", "-u0027"))
+        replaceText = m.replace("'", "-u0027")
       } else if (pattern == pattern3) {
-        txtOr = txtOr.replace(m, m.replace("\"", "-u0022"))
+        replaceText = m.replace("\"", "-u0022")
       }
+      txtOr = txtOr.replace(textOriginal, replaceText)
     }
     txtOr
   }
 
   val crew = data
-    .map(elem => (elem("id"), elem("crew").map(text => replacePattern4(text, pattern2))
+    .map(elem => (elem("id") ->
+      elem("crew")))
+    .map(_._2)
+      .map(text => replacePattern4(text, pattern2))
       .map(text => replacePattern4(text, pattern1))
       .map(text => replacePattern4(text, pattern3))
       .map(text => text.replace("'", "\""))
@@ -154,7 +148,7 @@ object SentenciasInsertInto extends App {
         x("department").as[String],
         x("job").as[String],
         x("credit_id").as[String],
-        x("id").as[Int]))))
+        x("id").as[Int]))
 
   println(crew)
   /*idMovieCrew.map(elem =>
