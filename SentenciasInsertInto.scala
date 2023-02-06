ea87a184ed407384a4caad83b8ce3c474ac17063
CarlosMC2003 <92834288+CarlosMC2003@users.noreply.github.com>
2023-02-05 20:41:38 -0500

    SENTENCIAS INSERT INTO - COMPLETADO
    Se culmino con la inserción de datos revisando que todos los datos hayan migrado con éxito.

--- SentenciasInsertInto.scala
+++ SentenciasInsertInto.scala
@@ -74,102 +74,108 @@ object SentenciasInsertInto extends App {
       .apply())*/
 
   //COLUMNA production_companies
-  /*val productionCompanies = data
-    .map(elem => (elem("id"), elem("production_companies")))
+  /*val Movie_production_companies = data
+      .map(x =>
+        (x("id"), Json.parse(x("production_companies")), Json.parse(x("production_companies"))))
+      .map(x => (x._1, x._2 \\ "id", x._3 \\ "name"))
+      .filter(x => x._2.nonEmpty && x._3.nonEmpty)
+      .flatMap(x => x._2.zip(x._3).map((x._1, _)))
+      .map(x => (x._1, x._2._1.as[Int], x._2._2.as[String]))
 
-  println(productionCompanies)
-
-  productionCompanies.map(elem =>
+  Movie_production_companies.map(elem =>
     sql"""
          |INSERT INTO production_companies (idMovie, name, idCompany)
          |VALUES
-         |(${elem._1}, ${Json.parse(elem._2) \\ "name"}, ${Json.parse(elem._2) \\ "id"})
+         |(${elem._1}, ${elem._3}, ${elem._2})
                      """.stripMargin
       .update
       .apply())*/
 
    //COLUMNA production_countries
-  /*val productionCountries = data
-    .map(elem => (elem("id"), elem("production_countries")))
+   /*val Movie_production_countries = data
+     .map(x =>
+       (x("id"), Json.parse(x("production_countries")), Json.parse(x("production_countries"))))
+     .map(x => (x._1, x._2 \\ "iso_3166_1", x._3 \\ "name"))
+     .filter(x => x._2.nonEmpty && x._3.nonEmpty)
+     .flatMap(x => x._2.zip(x._3).map((x._1, _)))
+     .map(x => (x._1, x._2._1.as[String], x._2._2.as[String]))
 
-  println(productionCountries)
-
-  productionCountries.map(elem =>
+  Movie_production_countries.map(elem =>
     sql"""
          |INSERT INTO production_countries (idMovie, name, `iso_3166_1`)
          |VALUES
-         |(${elem._1}, ${Json.parse(elem._2) \\ "name"}, ${Json.parse(elem._2) \\ "iso_3166_1"})
+         |(${elem._1}, ${elem._3}, ${elem._2})
                          """.stripMargin
       .update
       .apply())*/
 
   //COLUMNA spoken_language
   /*val spoken_language = data
-    .map(elem => (elem("id"), elem("spoken_languages")))
-
-  println(spoken_language)
+    .map(x =>
+      (x("id"), Json.parse(x("spoken_languages")), Json.parse(x("spoken_languages"))))
+    .map(x => (x._1, x._2 \\ "iso_639_1", x._3 \\ "name"))
+    .filter(x => x._2.nonEmpty && x._3.nonEmpty)
+    .flatMap(x => x._2.zip(x._3).map((x._1, _)))
+    .map(x => (x._1, x._2._1.as[String], x._2._2.as[String]))
 
   spoken_language.map(elem =>
     sql"""
-         |INSERT INTO spoken_language (idMovie, `iso_639_1`, name)
+         |INSERT INTO spoken_language (idMovie, iso_639_1, `name`)
          |VALUES
-         |(${elem._1}, ${Json.parse(elem._2) \\ "iso_639_1"}, ${Json.parse(elem._2) \\ "name"})
-                     """.stripMargin
+         |(${elem._1}, ${elem._2}, ${elem._3})
+                           """.stripMargin
       .update
       .apply())*/
 
   //COLUMNA Crew
-  val pattern1 = "(\\s\"(.*?)\",)".r
+  /*val pattern1 = "(\\s\"(.*?)\",)".r
   val pattern2 = "([a-z]\\s\"(.*?)\"\\s*[A-Z])".r
   val pattern3 = "(:\\s'\"(.*?)',)".r
 
   def replacePattern4(original: String, pattern: Regex): String = {
     var txtOr = original
-
     for (m <- pattern.findAllIn(original)) {
-      val textOriginal = m
-      var replaceText = m
       if (pattern == pattern2) {
-        replaceText = m.replace("\"", "-u0022")
+        txtOr = txtOr.replace(m, m.replace("\"", "-u0022"))
       } else if (pattern == pattern1) {
-        replaceText = m.replace("'", "-u0027")
+        txtOr = txtOr.replace(m, m.replace("'", "-u0027"))
       } else if (pattern == pattern3) {
-        replaceText = m.replace("\"", "-u0022")
+        txtOr = txtOr.replace(m, m.replace("\"", "-u0022"))
       }
-      txtOr = txtOr.replace(textOriginal, replaceText)
     }
     txtOr
   }
 
   val crew = data
-    .map(elem => (elem("id") ->
-      elem("crew")))
-    .map(_._2)
+    .map(row => row("crew"))
     .map(text => replacePattern4(text, pattern2))
     .map(text => replacePattern4(text, pattern1))
     .map(text => replacePattern4(text, pattern3))
     .map(text => text.replace("'", "\""))
     .map(text => text.replace("-u0027", "'"))
     .map(text => text.replace("-u0022", "\\\""))
-    .map(text => Try(Json.parse(text)) match {
-      case Success(i) => i
-    })
-    .flatMap(_.as[List[JsValue]])
-    .map(x => (x("name").as[String],
-      x("gender").as[Int],
-      x("department").as[String],
-      x("job").as[String],
-      x("credit_id").as[String],
-      x("id").as[Int]))
 
-  print(crew)
-  /*idMovieCrew.map(elem =>
+
+  val id = data
+    .map(row => row("id"))
+
+  val crewId = id.zip(crew)
+    .map(x =>
+      (x._1, Json.parse(x._2), Json.parse(x._2), Json.parse(x._2), Json.parse(x._2), Json.parse(x._2), Json.parse(x._2)))
+    .map(x => (x._1, x._2 \\ "name", x._3 \\ "gender", x._4 \\ "department", x._5 \\ "job", x._6 \\ "credit_id", x._7 \\ "id"))
+    .filter(x => x._2.nonEmpty && x._3.nonEmpty)
+    .flatMap(x => x._2.zip(x._3).zip(x._4).zip(x._5).zip(x._6).zip(x._7).map((x._1, _)))
+    .map{ case (id, (((((name, gender), department), job), creditId), personId)) =>
+      (id, name.as[String], gender.as[Int], department.as[String], job.as[String], creditId.as[String], personId.as[Int])
+    }
+
+  crewId.map(elem =>
     sql"""
          |INSERT INTO Crew (idMovie, id, name, gender, departament, job, credit_id)
          |VALUES
-         |(${elem._1}, ${Json.parse(elem._2) \\ "id"}, ${Json.parse(elem._2) \\ "name"}, ${Json.parse(elem._2) \\ "gender"}, ${Json.parse(elem._2) \\ "departament"}, ${Json.parse(elem._2) \\ "job"}, ${Json.parse(elem._2) \\ "credit_id"})
+         |(${elem._1}, ${elem._7}, ${elem._2}, ${elem._3}, ${elem._4}, ${elem._5}, ${elem._6})
                      """.stripMargin
       .update
-      .apply())*/
-
+      .apply())
+*/
 }
